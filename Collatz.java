ceedf0ac927ad02dc0dde93ea1ace7b1e91ee622
yehonatan gutman <gutman.yehonatan@post.runi.ac.il>
2024-11-22 00:34:58 +0200

    done

--- Collatz.java
+++ Collatz.java
@@ -26,5 +26,7 @@ public class Collatz {
             
             System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
         }
+		System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
+		
     }
 }
