6b1ba8011c5ec43a1c977b1579b4f02fb03cc983
yehonatan gutman <gutman.yehonatan@post.runi.ac.il>
2024-11-21 23:55:23 +0200

    done

--- Cheers.java
+++ Cheers.java
@@ -1,29 +1,30 @@
-
 public class Cheers {
         public static void main(String[] args) {
-	String name = args[0].toUpperCase();
-        int N = Integer.parseInt(args[1]);
-        int lengthName = name.length();
-        String check = "AEFHILMNORSX";
+            String name = args[0].toUpperCase();  
+            int N = Integer.parseInt(args[1]);   
+            int lengthName = name.length();      
+            String check = "AEFHILMNORSX";        
 
-        for (int i = 0; i < lengthName; i++) {
-        String add;
-        char a = name.charAt(i);
-        if (check.indexOf(a)>=0) {
-        add = "an";
-        } else {
-        add = "a";
+            
+            for (int i = 0; i < lengthName; i++) {
+                String add;
+                char a = name.charAt(i);  
+                if (check.indexOf(a) >= 0) {
+                    add = "an";  
+                } else {
+                    add = "a";   
+                }
+    
+                System.out.println("Give me" + " " + add + " " + a + ":" + " " + a + "!");
+            }
+    
+          
+            System.out.println("What does that spell?");
+    
+           
+            for (int i = 0; i < N; i++) {
+                System.out.println(name + "!!!");
+            }
         }
-
-        System.out.println("Give me" + " " + add + " " + a + ":" + " " + a + "!" );         
-
-}
-        System.out.println("What does that spell?");
-        for (int i = 0; i < N; i++){
-        System.out.println(args[0].toUpperCase()+ "!!!"); 
-
-        }
-
-
-        }
-}
+    }
+    
